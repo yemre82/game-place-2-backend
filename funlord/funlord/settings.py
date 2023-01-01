94722ca1459d00b8fe49891ae0b41ea44478ba68
tahakoner <tahakoner@gmail.com>
2023-01-02 00:59:59 +0300

    refactor: allow_host configured2

--- funlord/funlord/settings.py
+++ funlord/funlord/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'rest_framework',
-    'corseheaders'
+    'corseheaders',
     'users',
     'superuser',
     'personel'
