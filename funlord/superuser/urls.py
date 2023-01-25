75ae5ca33de4c6951996e2b1af100fb26eff761f
tahakoner <tahakoner@gmail.com>
2023-01-25 19:01:55 +0300

    feature:superuser views.py get_all_news, get_news apis added and other apis configured

--- funlord/superuser/urls.py
+++ funlord/superuser/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from superuser.views import  get_all_news,get_news, super_user_change_password, super_user_login
+from superuser.views import general_type_login,super_user_forgot_password,super_user_forgot_password_validation,super_user_change_password,get_all_news,get_news
 urlpatterns= [
-    # path('add-news',add_news),
+    path('general-type-login',general_type_login),
+    path('super-user-change-password',super_user_change_password),
+    path('super-user-forgot-password',super_user_forgot_password),
+    path('super-user-forgot-password-validation',super_user_forgot_password_validation),
     path('get-all-news',get_all_news),
-    path('get-news',get_news),
-    # path('edit-news',edit_news),
-    # path('delete-news',delete_news),
-    path('super-user-login',super_user_login),
-    path('super-user-change-password',super_user_change_password)
+    path('get-news/<int:news_id>',get_news)
 ]
--- funlord/superuser/views.py
+++ funlord/superuser/views.py
@@ -3,97 +3,14 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authtoken.models import Token
 from django.core.exceptions import ObjectDoesNotExist
 from funlord.responses import response_200, response_400
-from users.models import CustomUser,OTPForgotPassword,OTPRegister
-from users.utils import is_verified, register_parameters,email_exist,generate_random_num,is_password_match,password_is_valid,send_email, sendSMSVerification
-from superuser.models import Branchs, Company, News
-
-
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# def add_news(request):
-#     if not request.user.is_admin:
-#         return response_400('you can not add news')
-#     company_name=request.data.get('company_name')
-#     try:
-#         com_obj=Company.objects.get(name=company_name)
-#     except ObjectDoesNotExist as e:
-#         return response_400('there is no such company')
-#     title=request.data.get('title')
-#     short_description=request.data.get('short_description')
-#     description=request.data.get('description')
-#     image=request.data.get('image')
-#     is_campaign=request.data.get('is_campaign')
-#     price=request.data.get('price')
-#     News.objects.create(
-#         company=com_obj,
-#         title=title,
-#         short_description=short_description,
-#         description=description,
-#         image=image,
-#         is_campaign=is_campaign,
-#         price=price
-#     )
-#     return response_200('success')
-
-
-# api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-# def get_news(request,news_id):
-#     if not email_exist(request.user.email):
-#         return response_400('there is no such user')
-#     try:
-#         news_obj=News.objects.get(id=news_id)
-#     except ObjectDoesNotExist as e:
-#         return response_400('there no such news')
-#     return_obj={
-#         "id":news_obj.id,
-#         "title": news_obj.title,
-#         "short_description": news_obj.short_description,
-#         "description": news_obj.description,
-#         "image": "/media/"+str(news_obj.image),
-#         "created_at":news_obj.created_at
-#     }
-#     return response_200(return_obj)
-
-
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# def edit_news(request,news_id):
-#     if not email_exist(request.user.email):
-#         return response_400('there is no such user')
-#     try:
-#         news_obj=News.objects.get(id=news_id)
-#     except ObjectDoesNotExist as e:
-#         return response_400('there no such news')
-#     title=request.data.get('title')
-#     short_description=request.data.get('short_description')
-#     description=request.data.get('description')
-#     image=request.data.get('image')
-#     news_obj.title=title
-#     news_obj.short_description=short_description
-#     news_obj.description=description
-#     news_obj.image=image
-#     news_obj.save()
-#     return response_200('success')
-
-
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# def delete_news(request):
-#     if not email_exist(request.user.email):
-#         return response_400('tehre is no such user')
-#     id=request.data.get('id')
-#     try:
-#         news_obj=News.objects.get(id=id)
-#     except ObjectDoesNotExist as e:
-#         return response_400('there is no such news')
-#     news_obj.delete()
-#     return response_200('success')
+from users.models import CustomUser,OTPForgotPassword
+from users.utils import is_verified,generate_random_num,sendSMSVerification
+from superuser.models import Company, News
 
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-def super_user_login(request):
+def general_type_login(request):
     username = request.data.get("username")
     try:
         user_obj = CustomUser.objects.get(username=username)
@@ -141,7 +58,7 @@ def super_user_forgot_password(request):
     except ObjectDoesNotExist as e:
         return response_400('this user does not valid')
     otp=generate_random_num()
-    # sendSMSVerification(otp,[tel_no])
+    sendSMSVerification(otp,[tel_no])
     OTPForgotPassword.objects.create(
         user=request.user,
         otp=otp,
@@ -177,34 +94,51 @@ def super_user_forgot_password_validation(request):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def get_all_news(request,filtering):
-    if not is_verified(request.user.tel_no):
-        return response_400('there is no such user')
-    list=[]
-    if filtering == 'all':
-        news_obj=News.objects.all().order_by('-id')
-    if filtering != 'all':
-        try:
-            company_obj=Company.objects.get(company=filtering)
-        except ObjectDoesNotExist as e:
-            return response_400('there is no such company')
-        for i in news_obj:
-            return_obj={
-                'id':i.id,
-                'title':i.title,
-                'short_description':i.short_description,
-                'description':i.description,
-                'image': '/media/'+str(i.image),
-                'is_campaign':i.is_campaign,
-                'price':i.price,
-                'created_at':i.created_at
-            }
-            list.append(return_obj)
+def get_all_news(request):
+    if not request.user.is_admin:
+        return response_400('this user is not admin')
+    if not request.user.is_can_write:
+        return response_400('this user is not a superuser')
+    news_obj = News.objects.all().order_by('-id')
+    list = []
+    for i in news_obj:
+        return_obj = {
+            'id': i.id,
+            'title': i.title,
+            'short_description': i.short_description,
+            'description': i.description,
+            'image': '/media/'+str(i.image),
+            'is_campaign': i.is_campaign,
+            'created_at': i.created_at,
+            'price': i.price,
+        }
+        list.append(return_obj)
     return response_200(list)
 
-    
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_news(request, news_id):
+    if not is_verified(request.user.tel_no):
+        return response_400('this user does not valid')
+    try:
+        news_obj = News.objects.get(id=news_id)
+    except ObjectDoesNotExist as e:
+        return response_400('there is no such news')
+    list = []
+    return_obj = {
+        'id': news_obj.id,
+        'title': news_obj.title,
+        'short_description': news_obj.short_description,
+        'description': news_obj.description,
+        'image': '/media/' + str(news_obj.image),
+        'is_campaign': news_obj.is_campaign,
+        'price': news_obj.price,
+    }
+    list.append(return_obj)
+    return response_200(list)
 
+ 
 
 
 
